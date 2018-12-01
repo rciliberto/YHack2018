e77cfbd8c6af904e5ec6242a14de6b339c84ae95
David Shen <me@pantherman594.com>
2018-12-01 02:44:59 -0500

    Update structure

--- generator.py
+++ generator.py
@@ -1,6 +1,8 @@
+from mido import Message, MidiFile, MidiTrack
 import random
+
 def generate_model(file_name):
-    # returns dictionary { note: [ [note, freq], [note, freq], ... ], ... }
+    # returns dictionary { Tick: [ [ Tick, freq], ... ], ... }
     return
 
 def get_next_note(prev_note, model):
@@ -19,49 +21,63 @@ def get_next_note(prev_note, model):
 def generate_new(model):
     prev_note = 'FIRST'
     ticks = []
-    while ((next_note = get_next_note(prev_note, model)) != 'END'):
+    next_note = get_next_note('FIRST', model)
+    while (next_note != 'END'):
         ticks.append(next_note)
         prev_note = next_note
+        next_note = get_next_note(prev_note, model)
     return ticks
 
 def save(song, file_name):
     mid = MidiFile(type=0)
     track = MidiTrack()
     mid.tracks.append(track)
+    track.append(Message('program_change', program=1, time=0))
 
-    tick = 0
-    last_command = 0
+    curr_tick = 1
+    last_command = 1
     running_notes = []
     for tick in song:
-        tick ++ 1
         to_keep = []
         to_delete = []
 
         for note in running_notes:
-            if note in tick.notes:
+            if note in tick:
                 to_keep.append(note)
             else:
                 to_delete.append(note)
 
-        for note in tick.notes:
+        for note in tick:
             if not note in to_keep:
                 to_keep.append(note)
-		track.append(Message('note_on',
-                    note=note.note,
-                    velocity=note.velocity,
-                    time=(tick - last_command)
+                track.append(Message('note_on',
+                    note=note,
+                    velocity=127,
+                    time=(curr_tick - last_command)
                 ))
-                last_command = tick
+                print('note_on ' + str(note) + ' ' + str(curr_tick - last_command))
+                last_command = curr_tick
 
         running_notes = to_keep
         for note in to_delete:
             track.append(Message('note_off',
-                note=note.note,
+                note=note,
                 velocity=127,
-                time=(tick - last_command)
+                time=(curr_tick - last_command)
             ))
-            last_command = tick
+            print('note_off ' + str(note) + ' ' + str(curr_tick - last_command))
+            last_command = curr_tick
 
+        curr_tick += 1
+
+    for note in running_notes:
+        track.append(Message('note_off',
+            note=note.note,
+            velocity=127,
+            time=(curr_tick - last_command)
+        ))
+        print('note_off ' + str(note) + ' ' + str(curr_tick - last_command))
+        last_command = curr_tick
     mid.save(file_name)
 
 model = generate_model('./01Minuetto1.mid')
