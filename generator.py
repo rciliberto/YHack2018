4c55c0a2ebd1e950caadcce825d957c7e1dfb634
Benjamin Li <pharmashawarma@gmail.com>
2018-11-30 22:51:37 -0500

    Added get_next_note functionality

--- generator.py
+++ generator.py
@@ -1,10 +1,19 @@
-def generate_model(fileName):
+import random
+def generate_model(file_name):
     # returns dictionary { note: [ [note, freq], [note, freq], ... ], ... }
     return
 
-def get_next_note(prevNote, model):
+def get_next_note(prev_note, model):
     # returns next note based on previous
-    return
+    sum = 0
+    for nested_list in note_list:
+        sum += nested_list[1]
+    weighted_val = random.randint(0,sum)
+    note_list = model[prev_note]
+    for nested_list in note_list:
+        sum -= weighted - (nested_list[1]-1)
+        if sum <= 0:
+            return nested_list[0]
 
 def generate_new(model):
     # output: newly generated midi file
