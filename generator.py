494e41963a53201b97785728a85b319b144451c6
David Shen <me@pantherman594.com>
2018-11-30 22:20:02 -0500

    Create skeleton

---
+++ generator.py
@@ -0,0 +1,11 @@
+def generate_model(fileName):
+    # returns dictionary { note: [ [note, freq], [note, freq], ... ], ... }
+    return
+
+def get_next_note(prevNote):
+    # returns next note based on previous
+    return
+
+def generate_new(model):
+    # output: newly generated midi file
+    return
